1dda0d1fe61763b1a1e2169272e0e138126ac345
linrichard99 <rlin5@stuy.edu>
2016-05-04 20:16:11 -0400

    add works now

--- ArrayPriorityQueue.java
+++ ArrayPriorityQueue.java
@@ -9,15 +9,37 @@ public class ArrayPriorityQueue {
     }
     
     public void add(Comparable x) {
-	boolean added = false;
-	for(int i = _queue.size() - 1; i >= 0; i--) {
-	    if(_queue.get(i).compareTo(x) >= 0) {
-		_queue.add(i, x);
-		added = true;
-		break;
+
+	//Insert directly into queue if empty
+	if (_queue.size() == 0) {
+	    _queue.add(x);
+	    System.out.println("" + x + this);
+	    return;
+	}
+
+	//This is very similar to the insertion sort algo
+	int insert = _queue.size(); //This stores the insert point. Note how it starts at one plus the final index, which is used if no values are greater than the inserted one.
+	Comparable comp = _queue.get(insert-1); //This is the value we're comparing x to
+
+	//Very standard "insertion sort" back traversal, where you walk down the compared value until you reach something that is smaller than it.
+	while (insert > 0 && x.compareTo(comp) < 0) {
+	    //System.out.println("comp: " + comp + "val: " + x.compareTo(comp));
+	    insert--;
+	    //This line is to compensate for the fact that we started insert at one plus the index, and so we may have an out of bounds error. 
+	    if (insert > 0) {
+		comp = _queue.get(insert-1);
 	    }
 	}
-	if(!added) _queue.add(x);
+	
+	//If nothing has changed with insert, just add to the end
+	if (insert == _queue.size()) {
+	    _queue.add(x);
+	}
+	//Else, add at that point.
+	else {
+	    _queue.add(insert,x);
+	}
+	//System.out.println("" + x + this);
     }
 
     public boolean isEmpty() {
